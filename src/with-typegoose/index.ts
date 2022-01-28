a711c900a88b272fbeb7fda5ce9688ad606c9cc2
linbudu599 <linbudu599@gmail.com>
2022-01-28 17:49:05 +0800

    feat: bump versions

--- src/with-typegoose/index.ts
+++ src/with-typegoose/index.ts
@@ -22,67 +22,72 @@ class TypeGooseValue {
 const ValueModel = getModelForClass(TypeGooseValue);
 
 async function main() {
-  const connection = await mongoose.connect(
-    process.env.WITH_TYPEGOOSE_TYPEGOOSE_VALUE_URL!,
-    { dbName: "prisma" }
-  );
+  try {
+    const connection = await mongoose.connect(
+      process.env.WITH_TYPEGOOSE_TYPEGOOSE_VALUE_URL!,
+      { dbName: "prisma" }
+    );
 
-  await ValueModel.deleteMany({});
-  await prisma.prismaKey.deleteMany();
+    await ValueModel.deleteMany({});
+    await prisma.prismaKey.deleteMany();
 
-  // seeding
-  const key1 = await prisma.prismaKey.create({
-    data: {
-      key: uuidv4(),
-    },
-    select: {
-      key: true,
-    },
-  });
-  console.log("key1: ", key1);
+    // seeding
+    const key1 = await prisma.prismaKey.create({
+      data: {
+        key: uuidv4(),
+      },
+      select: {
+        key: true,
+      },
+    });
+    console.log("key1: ", key1);
 
-  const key2 = await prisma.prismaKey.create({
-    data: {
-      key: uuidv4(),
-    },
-    select: {
-      key: true,
-    },
-  });
-  console.log("key2: ", key2);
+    const key2 = await prisma.prismaKey.create({
+      data: {
+        key: uuidv4(),
+      },
+      select: {
+        key: true,
+      },
+    });
+    console.log("key2: ", key2);
 
-  const value1 = await ValueModel.create({
-    key: key1.key,
-    value: "林不渡",
-  });
-
-  console.log("value1: ", value1);
-
-  const value2 = await ValueModel.create({
-    key: key2.key,
-    value: "林不渡",
-  });
-
-  console.log("value2: ", value2);
-
-  // query
-  const keys: PrismaKey[] = await prisma.prismaKey.findMany();
-
-  for (const keyItem of keys) {
-    const key = keyItem.key;
-
-    console.log(`Search By: ${key}`);
-
-    const value = await ValueModel.findOne({
-      key,
+    const value1 = await ValueModel.create({
+      key: key1.key,
+      value: "林不渡",
     });
 
-    console.log("Search Result: ", value);
-    console.log("===");
-  }
+    console.log("value1: ", value1);
 
-  await connection.disconnect();
-  await prisma.$disconnect();
+    const value2 = await ValueModel.create({
+      key: key2.key,
+      value: "林不渡",
+    });
+
+    console.log("value2: ", value2);
+
+    // query
+    const keys: PrismaKey[] = await prisma.prismaKey.findMany();
+
+    for (const keyItem of keys) {
+      const key = keyItem.key;
+
+      console.log(`Search By: ${key}`);
+
+      const value = await ValueModel.findOne({
+        key,
+      });
+
+      console.log("Search Result: ", value);
+      console.log("===");
+    }
+
+    await connection.disconnect();
+    await prisma.$disconnect();
+  } catch (error) {
+    console.warn("\nAre u sure MongoDB is running?\n\n");
+    console.warn(error);
+  }
 }
 
 main();
